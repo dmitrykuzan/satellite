fc61fd31a926b598fbbfc31da102861ea913191c
dmitry <dmitrykuzan@gmail.com>
2022-04-12 13:09:05 +0300

    ready all section

--- src/js/_vendor.js
+++ src/js/_vendor.js
@@ -1,11 +1,6 @@
 // Библиотека для фокуса (доступности)
 import "./vendor/focus-visible.js";
 
-import "../../node_modules/swiper/swiper-bundle";
-import Swiper, {  Navigation, Pagination} from 'swiper';
-Swiper.use([Navigation, Pagination]);
-const swiper = new Swiper();
-
 // Подключение плавной прокрутки к якорям (подключать в случае если плавная прокрутка нужна в Safari)
 // import SmoothScroll from 'smooth-scroll';
 // const scroll = new SmoothScroll('a[href*="#"]');
